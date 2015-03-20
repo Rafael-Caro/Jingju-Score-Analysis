7a1b1a29a455a47a1e45c9d13a496501aec670d8
Rafael Caro Repetto <rafael.caro.repetto@gmail.com>
2015-03-20 09:03:19 +0100

    Added a new function for metrical analysis

--- jingjuScoreAnalysis.py
+++ jingjuScoreAnalysis.py
@@ -12,7 +12,8 @@ def ejemplo():
     x.insert(4.0, adorno)
     return x
 
-def weightedHistogramPitchSpace(stream, width = 0.4, count = 'abs'):
+def weightedHistogramPitchSpace(stream, width=.4, count = 'abs', figsize=None,
+                                fontsize=None):
     '''(music21.stream.Score) -> matplotlib.pyplot.bar, dict
     Plots a pitch histogram for stream and returns a dictionary with
     the note classes as keys and their agregate duration as value.
@@ -62,15 +63,22 @@ def weightedHistogramPitchSpace(stream, width = 0.4, count = 'abs'):
         step = interval.notesToChromatic(n1, n2).directed
         xposition = np.append(xposition, xposition[-1] + step)
 
+    plt.figure(1, figsize)
     plt.bar(xposition, values, width)
-    plt.title('Weighted Histogram of Pitch Space')
-    plt.ylabel(ylabel)
-    plt.xticks(xposition + width/2, sortedNotes)
+    plt.title('Weighted Histogram of Pitch Space', fontsize=fontsize)
+    plt.xlabel('Pitch', fontsize=fontsize)
+    plt.ylabel(ylabel, fontsize=fontsize)
+    if fontsize != None:
+        plt.xticks(xposition + width/2, sortedNotes, fontsize=(fontsize*0.6))
+    else:
+        plt.xticks(xposition + width/2, sortedNotes)
+    plt.grid(True, axis='y')
     plt.show()
 
     return noteDur
 
-def intervalHistogram(stream, width = 0.4, directed=False, count='abs'):
+def intervalHistogram(stream, width=.4, directed=False, count='abs',
+                      figsize=None, fontsize=None):
     '''(music21.stream.Score) -> matplotlib.pyplot.bar, dict
     Plots a histograms of the intervals found in stream and returns a
     dictionary with the interval classes as keys and their total count
@@ -120,10 +128,13 @@ def intervalHistogram(stream, width = 0.4, directed=False, count='abs'):
     xposition = np.array(sorted([intSteps[i] for i in sortedInt]))
 
     # Plotting the bar chart
+    plt.figure(1, figsize)
     plt.bar(xposition, intValues, width)
-    plt.title('Intervals Histogram')
-    plt.ylabel(ylabel)
-    plt.xticks(xposition + width/2, sortedInt)
+    plt.title('Histogram of Intervals', fontsize=fontsize)
+    plt.xlabel('Interval class', fontsize=fontsize)
+    plt.ylabel(ylabel, fontsize=fontsize)
+    plt.xticks(xposition + width/2, sortedInt, fontsize=(fontsize*.6))
+    plt.grid(True, axis='y')
     plt.show()
 
     return intCount
@@ -178,7 +189,7 @@ def beatCountPerNote(pitchClass, stream, showScore=True):
     else:
         beat_dic = {}
         for n in found_notes:
-            beat_class = n.beat # This number has tu be changed to the time signature's numerator
+            beat_class = n.beat # This number has to be changed to the time signature's numerator
             if beat_class not in beat_dic:
                 beat_dic[beat_class] = 1
             else:
@@ -200,11 +211,21 @@ def beatCountPerNote(pitchClass, stream, showScore=True):
 
     return beat_classes, beat_values, beat_ticks
 
-def beatHistogram(stream, tonic=None, width=0.1, rotation=45):
-    pitch_classes = []
+def beatHistogram(stream, tonic=None, width=.4, rotation=45, figsize=None,
+                  fontsize=None):
+    pitches = []
     for n in stream.flat.notes:
-        if n.name not in pitch_classes:
-            pitch_classes.append(n.name)
+        if n.name not in pitches:
+            pitches.append(n.name)
+
+    # Organize pitches starting from the given tonic, if any
+    pitches.sort()
+    if tonic != None:
+        ton_ind = pitches.index(tonic)
+        pitch_classes = pitches[ton_ind:]
+        pitch_classes.extend(pitches[:ton_ind])
+    else:
+        pitch_classes = pitches
 
     plots = len(pitch_classes)
     rows = (plots / 2) + (plots % 2)
@@ -217,17 +238,98 @@ def beatHistogram(stream, tonic=None, width=0.1, rotation=45):
         maxima.append(max(y))
     ylim = max(maxima)
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=figsize)
+    ax0 = fig.add_subplot(111)
+    for i in ['top', 'bottom', 'left', 'right']:
+        ax0.spines[i].set_color('none')
+    ax0.tick_params(labelcolor='w', top='off', bottom='off', left='off',
+                    right='off')
+    ax0.set_xlabel('Beat', fontsize=fontsize)
+    ax0.set_ylabel('Count', fontsize=fontsize)
     for i in range(len(pitch_classes)):
         x, y, t = beatCountPerNote(pitch_classes[i], stream, showScore=False)
-        ax = fig.add_subplot(rowsAndCol + i)
-        ax.bar(x, y, width)
+        ax = fig.add_subplot(rowsAndCol + i + 1)
+        ax.bar(x, y, width=width)
         ax.set_ylim(0, ylim)
         ax.set_xlim(1, 3)
+#        ax.yaxis.set_ticks(np.arange(0, ylim, 3))
         ax.set_xticks(np.array(x) + width/2)
         ax.set_xticklabels(t, rotation=rotation)
-        ax.set_title(pitch_classes[i])
+        ax.set_title(pitch_classes[i], fontsize=(fontsize*.75))
+        ax.grid(True, axis='y')
 
     fig.tight_layout()
     plt.show()
     
+def timeSignatureMeasures(timeSignature, source):
+    '''(str, music21.stream) --> list of lists
+    Returns a list of lists containing the start measure number and the end measure number
+    for each section in source that is set to timesignature
+    '''
+    timeSignatures = source.flat.getTimeSignatures()
+    measureNumbers = []
+    for i in timeSignatures:
+        if i.ratioString == timeSignature:
+            ini = i.measureNumber
+            end = timeSignatures[timeSignatures.index(i)+1].measureNumber-1
+            measureNumbers.append([ini, end])
+
+    return measureNumbers
+    
+def getSectionByTimeSignature(timeSignatureList, source):
+    '''(list, music21.stream) --> music21.stream
+    Given a list conatining lists with the start measure number and the end measure number
+    for each section in a specific time Signature, it returns a stream containing all
+    those sections from source
+    '''
+    section = stream.Stream()
+    for i in timeSignatureList:
+        for j in source.measures(i[0], i[1]).getElementsByClass('Measure'):
+            section.append(j)
+
+    return section
+
+def beatAnalysis(stream, beatList, tonic=None, width=.4, figsize=None,
+                 fontsize=None):
+    
+    beats = {}
+    for n in stream.flat.notes:
+        if n.duration.quarterLength > 0:
+            if n.name not in beats:
+                beats[n.name] = [n.beat]
+            else:
+                beats[n.name].append(n.beat)
+
+    count = {}
+    for i in beats:
+        count[i] = []
+        for j in beatList:
+            count[i].append(beats[i].count(j))
+
+    pitches = [i for i in beats]
+
+    # Organize pitches starting from the given tonic, if any
+    pitches.sort()
+    if tonic != None:
+        ton_ind = pitches.index(tonic)
+        pitch_classes = pitches[ton_ind:]
+        pitch_classes.extend(pitches[:ton_ind])
+    else:
+        pitch_classes = pitches
+
+    #return beats, count, pitch_classes
+
+    ind = np.arange(len(beatList))
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+    bottom = np.zeros(len(ind))
+
+    plots = []
+    for i in range(len(pitch_classes)):
+        height = count[pitch_classes[i]]
+        plots.append(plt.bar(ind, height, width, bottom=bottom,
+                color=colors[i]))
+        bottom += height
+    plt.xticks(ind+width/2., [str(i) for i in beatList])
+    plt.legend(plots, pitch_classes)
+
+    plt.show()
